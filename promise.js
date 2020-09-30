7ab7a4ff0b644f6483af7cbe5d087de6cea933fb
Visnyei Ádám <visnyei.adam@szbisz.local>
2020-09-30 15:17:09 +0200

    harmadik

---
+++ promise.js
@@ -0,0 +1,39 @@
+'use strict' ;
+
+const posts = [
+    {'title': 'post one', 'body': 'this is post one'},
+    {'title': 'post two', 'body': 'this is post two'},
+];
+
+
+function getPosts() {
+    setTimeout(() =>{
+        let output = '';
+        posts.forEach((post, index) =>{
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);        
+}
+
+function createPost(post,){
+    return new Promise((resolve, reject) =>
+    {
+        setTimeout(() => {
+            posts.push(post);
+            const error = false;
+            if(!error){
+                resolve();
+            }
+            else
+            {
+                reject(`Valami elromlott!`);
+            }
+    });
+
+        
+    }, 2000);
+    
+} 
+
+createPost({'title' : 'Post three', 'body' : 'This is post three'}, getPosts);
